d44225f6860d6cf2964c45e711cbfd91a4055934
Mikel Bishop <mdrew8@gmail.com>
2020-08-28 21:38:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,7 @@ def my_collect(languages)
   i = 0 
   array = []
   while i < languages.length
-  array << languages.collect  do |lang|
-    lang.upcase
+  array << languages.upcase
   end
   array
 end
